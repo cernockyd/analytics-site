7fc046a5132c091886fe9f8121fdaa083853493b
cernockyd <24832873+cernockyd@users.noreply.github.com>
2022-11-07 18:12:05 +0100

    auto create book readers list materialized view

--- pages/api/refresh-books-readers-list.ts
+++ pages/api/refresh-books-readers-list.ts
@@ -4,6 +4,18 @@ import sql from 'lib/postgres';
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     await sql.unsafe(`
+      create materialized view if not exists books_readers_by_year
+      as
+        select
+          domain,
+          split_part(domain, 'books-are-next.github.io/', 2) as slug,
+          count(distinct client_id) as readers,
+          date_trunc('year', time) as year 
+        from events
+        where domain != '127.0.0.1:23011'
+        group by domain, year
+    `);
+    await sql.unsafe(`
       refresh materialized view books_readers_by_year
     `);
     res.status(200).json({ statusCode: 200 });
